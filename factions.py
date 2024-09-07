a7a9972560efd24a1bfc229ff42094872c5c29a9
Grandmaster <tamirlan.ramazanov@gmail.com>
2024-09-08 01:49:26 +0500

    a

---
+++ factions.py
@@ -0,0 +1,28 @@
+# factions.py
+
+# Global dictionary for storing factions
+FACTIONS = {
+    1: "Roger's Pirates",
+    2: "Whitebeard's Pirates",
+    3: "Yonko",
+    4: "Shichibukai",
+    5: "Worst Generation",
+    6: "Marines",
+    7: "Monkey family",
+    8: "Adopted and Honorary",
+    9: "Revolutionary Army",
+    10: "Red Hair Pirates",
+    11: "Cross Guild",
+    12: "Straw Hat Pirates",
+    13: "Kuja Pirates",
+    14: "Baroque Works",
+    15: "Fish-men",
+    16: "Father-Son",
+    17: "Skypiea",
+    18: "Kid Pirates",
+    19: "Baratie crew",
+    20: "Black Cat Pirates",
+    21: "Giants",
+    22: "Arlong's Pirates",
+    23: "Baratie Arc"
+}
